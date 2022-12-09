f1d8e6e3f35635ad019a9bedce796b3d38008d0b
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-09 15:13:26 +0530

    user registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { UserloginComponent } from './userlogin/userlogin.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminloginComponent,
-    UserloginComponent
+    UserloginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
