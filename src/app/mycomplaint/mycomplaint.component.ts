0ef0b5107490ed3a997889e78918d95a579c6321
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-10 11:02:58 +0530

    my complaint

---
+++ src/app/mycomplaint/mycomplaint.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycomplaint',
+  templateUrl: './mycomplaint.component.html',
+  styleUrls: ['./mycomplaint.component.css']
+})
+export class MycomplaintComponent {
+
+}
